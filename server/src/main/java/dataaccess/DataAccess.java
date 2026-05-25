46d854b934468abc6709a41770249f0d4f97530d
“Chloe <cwarner0@byu.edu>
2026-05-25 17:59:37 -0600

    Phase 4: Working on dataaccess, just did the public one in and had it generate the stibs for me for methods.

--- server/src/main/java/dataaccess/DataAccess.java
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -3,24 +3,92 @@ package dataaccess;
 import model.AuthData;
 import model.GameData;
 import model.UserData;
+
+import javax.xml.crypto.Data;
 import java.util.List;
 
 public interface DataAccess {
+
+
     // User methods
+    //THis one will need the username, the apssowrd and the email
+
     void createUser(UserData user) throws DataAccessException;
     UserData getUser(String username) throws DataAccessException;
 
     // Game methods
+    //this will need the authtoken adn the username
+
+
     int createGame(GameData game) throws DataAccessException;
     GameData getGame(int gameID) throws DataAccessException;
     List<GameData> listGames() throws DataAccessException;
     void updateGame(GameData game) throws DataAccessException;
 
     // Auth methods
+    //this will need the gameID, white username, black username and the gamename and the game json
+
+
     void createAuth(AuthData auth) throws DataAccessException;
     AuthData getAuth(String authToken) throws DataAccessException;
     void deleteAuth(String authToken) throws DataAccessException;
 
     // Clear
+
+
+
     void clear() throws DataAccessException;
+}
+
+public class SqlAccess implements DataAccess {
+
+    @Override
+    public void createUser(UserData user) throws DataAccessException {
+
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public int createGame(GameData game) throws DataAccessException {
+        return 0;
+    }
+
+    @Override
+    public GameData getGame(int gameID) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public List<GameData> listGames() throws DataAccessException {
+        return List.of();
+    }
+
+    @Override
+    public void updateGame(GameData game) throws DataAccessException {
+
+    }
+
+    @Override
+    public void createAuth(AuthData auth) throws DataAccessException {
+
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public void deleteAuth(String authToken) throws DataAccessException {
+
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+
+    }
 }
