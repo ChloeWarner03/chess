d56218925cfaabeb12f2d9e3c886ff3e5bd76c84
“Chloe <cwarner0@byu.edu>
2026-05-27 06:44:20 -0600

    Phase 4: working on the tests, need to mkae use that updateFakeGame is right

--- server/src/test/java/dataaccess/SqlTestsPersonal.java
+++ server/src/test/java/dataaccess/SqlTestsPersonal.java
@@ -5,12 +5,11 @@ import model.UserData;
 import org.junit.jupiter.api.*;
 import model.GameData;
 import chess.ChessGame;
+import model.AuthData;
 
 //can this be used
 import static org.junit.jupiter.api.Assertions.*;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class SqlTestsPersonal {
     private static SqlAccess data;
@@ -48,6 +47,7 @@ public class SqlTestsPersonal {
                 data.createUser(new UserData("chloe", "pass", "chloe@email.com")));
     }
     //grab the user
+    @Test
     void grabUser() throws DataAccessException {
         // add a user first
         data.createUser(new UserData("chloe", "pass", "chloe@email.com"));
@@ -80,8 +80,66 @@ public class SqlTestsPersonal {
         assertNotNull(data.getGame(gameID));
     }
     //Cannot find a game
+    @Test
     void getGameNotFound() throws DataAccessException {
         // game that doesnt exist should return null
         assertNull(data.getGame(123456789));
     }
+    //List Games: need Pos and Neg
+    //empty game list
+    @Test
+    void gameEmpty() throws DataAccessException {
+        // no games should return empty list
+        assertEquals(0, data.listGames().size());
+    }
+    @Test
+    void listGamesWorks() throws DataAccessException {
+        // add games
+        data.createGame(new GameData(0, null, null, "GAMENUMBER1", new ChessGame()));
+        data.createGame(new GameData(0, null, null, "GAMENUMBER2", new ChessGame()));
+        // needs 2 games
+        assertEquals(2, data.listGames().size());
+    }
+    //updateGame: need Pos and Neg
+    //UPdate a real game
+    @Test
+    void updateRealGame() throws DataAccessException {
+        //make a real gamemake a game
+        var gameID = data.createGame(new GameData(0, null, null, "RealGame", new ChessGame()));
+        // make player
+        data.updateGame(new GameData(gameID, "chloe", null, "RealGame", new ChessGame()));
+        //update worked
+        assertEquals("chloe", data.getGame(gameID).whiteUsername());
+    }
+    //Cant update a game that does not exist
+    @Test
+    void updateFakeGame() throws DataAccessException {
+        // game does not exist= fail
+        assertThrows(DataAccessException.class, () ->
+                data.updateGame(new GameData(99999, "chloe", null, "FakeGame", new ChessGame())));
+    }
+    //createAuth need Pos and Neg
+    @Test
+    void createAuthentification() throws DataAccessException {
+        // make an auth token
+        data.createAuth(new AuthData("token378", "chloe"));
+        // make sure it is there
+        assertNotNull(data.getAuth("token378"));
+    }
+
+    @Test
+    void multipleAuth() throws DataAccessException {
+        // add a token
+        data.createAuth(new AuthData("token378", "chloe"));
+        // adding same token should fail
+        assertThrows(DataAccessException.class, () ->
+                data.createAuth(new AuthData("token378", "chloe")));
+    }
+    //getAuths: need Pos and
+
+
+    //deleteAuth: need Pos and Neg
+    //clear: pos
+
+
 }
