e8dae351cf32e4dd7ddb660b456750955bbf3b7c
“Chloe <cwarner0@byu.edu>
2026-05-26 18:10:27 -0600

    Phase 4: making the tests to make usre that everything is working corectly, not for jsut the etacher tests but for mine as well, trying to catch more stuff. I h=am working on the create user and userstuff tests

--- server/src/test/java/dataaccess/SqlTestsPersonal.java
+++ server/src/test/java/dataaccess/SqlTestsPersonal.java
@@ -1,4 +1,45 @@
 package dataaccess;
 
+//my imports
+import model.UserData;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class SqlTestsPersonal {
+    private static SqlAccess data;
+
+    //setting it al up for the tests
+    @BeforeAll
+    static void setitallup() throws DataAccessException {
+        data = new SqlAccess();
+    }
+
+    //This one clears the Database
+    @BeforeEach
+    void clearDatabase() throws DataAccessException {
+        // This will clear before the tests
+        data.clear();
+    }
+
+    //createUser: these will be the tests for creatUser
+    //create a user
+    @Test
+    void createanewUser() throws DataAccessException {
+        // add a user
+        data.createUser(new UserData("chloe", "password123", "chloe@email.com"));
+        // make sure they are there
+        assertNotNull(data.getUser("chloe"));
+    }
+
+    //This test will make it so then there is no duplicates
+    @Test
+    void sameUser() throws DataAccessException {
+        // add a user
+        data.createUser(new UserData("chloe", "pass", "chloe@email.com"));
+        // adding same user should fail
+        assertThrows(DataAccessException.class, () ->
+                data.createUser(new UserData("chloe", "pass", "chloe@email.com")));
+    }
 }
