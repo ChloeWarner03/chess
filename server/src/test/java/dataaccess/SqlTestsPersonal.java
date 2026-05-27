78aa96d1949d1acb30487c0fc1b493e0580d7ac9
“Chloe <cwarner0@byu.edu>
2026-05-27 06:23:55 -0600

    Phase 4: MAking more tests to make sure that everything is right and htat they all work

--- server/src/test/java/dataaccess/SqlTestsPersonal.java
+++ server/src/test/java/dataaccess/SqlTestsPersonal.java
@@ -3,6 +3,11 @@ package dataaccess;
 //my imports
 import model.UserData;
 import org.junit.jupiter.api.*;
+import model.GameData;
+import chess.ChessGame;
+
+//can this be used
+import static org.junit.jupiter.api.Assertions.*;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -41,5 +46,42 @@ public class SqlTestsPersonal {
         // adding same user should fail
         assertThrows(DataAccessException.class, () ->
                 data.createUser(new UserData("chloe", "pass", "chloe@email.com")));
+    }
+    //grab the user
+    void grabUser() throws DataAccessException {
+        // add a user first
+        data.createUser(new UserData("chloe", "pass", "chloe@email.com"));
+        // them find and grab them
+        assertNotNull(data.getUser("chloe"));
+    }
+    //These are going to be for the Game
+    //Create a new Game
+    @Test
+    void createNewGame() throws DataAccessException {
+        // make a new game
+        var gameID = data.createGame(new GameData(0, null, null, "testGame", new ChessGame()));
+        // should have a real id
+        assertTrue(gameID > 0);
+    }
+
+    //The Game needs to have a name
+    @Test
+    void gameNeedsName() throws DataAccessException {
+        // game with no name should fail
+        assertThrows(DataAccessException.class, () ->
+                data.createGame(new GameData(0, null, null, null, new ChessGame())));
+    }
+
+    //grab the game
+    @Test
+    void getGameWorks() throws DataAccessException {
+        // make a game then get it back
+        var gameID = data.createGame(new GameData(0, null, null, "testGame", new ChessGame()));
+        assertNotNull(data.getGame(gameID));
+    }
+    //Cannot find a game
+    void getGameNotFound() throws DataAccessException {
+        // game that doesnt exist should return null
+        assertNull(data.getGame(123456789));
     }
 }
