5c66773b759cd2635990628094b4c0dc89ea7125
“Chloe <cwarner0@byu.edu>
2026-06-03 06:49:21 -0600

    Phase 5: Need to do the rest of it but making the functions that I will need for the ChessBoard and getting it all set up so then I can finish making it all work

--- client/src/main/java/client/MakeChessBoard.java
+++ client/src/main/java/client/MakeChessBoard.java
@@ -1,9 +1,32 @@
 package client;
 import chess.ChessGame;
+import chess.ChessBoard;
+import static ui.EscapeSequences.*;
+
+
 
 public class MakeChessBoard {
-    public static void drawBoard(ChessGame game, boolean whitePerspective) {
-        System.out.println("board");//trying to see the errors that are coming up
+    public static void createChessBoard(ChessGame game, boolean seeBlackSide) {
+        ChessBoard board = game.getBoard();
+        createChessBoardCol(seeBlackSide);
+        if (seeBlackSide) {
+            for (int row = 1; row <= 8; row++) {
+                createChessBoardRow(board, row, false);
+            }
+        } else {
+            for (int row = 8; row >= 1; row--) {
+                createChessBoardRow(board, row, true);
+            }
+        }
 
+        createChessBoardCol(seeBlackSide);
     }
+
+    public static void createChessBoardCol(boolean seeBlackSide) {
+    }
+
+
+    public static void createChessBoardRow(ChessBoard board, int row, boolean whitePerspective) {
+    }
+    S
 }
