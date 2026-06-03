f973c8ec50a31d3d05c1d9dbd83e94dfb775539b
“Chloe <cwarner0@byu.edu>
2026-06-03 12:53:59 -0600

    Phase 5: code quality had a little problem thing

--- client/src/main/java/ui/MakeChessBoard.java
+++
@@ -1,9 +0,0 @@
-package ui;
-import chess.ChessGame;
-
-public class MakeChessBoard {
-    public static void drawBoard(ChessGame game, boolean whitePerspective) {
-        System.out.println("board");
-
-    }
-}
